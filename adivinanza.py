b756ade1e65aaf451a17008683f0c0f6e0cc46f5
aseoaneo23 <145000901+aseoaneo23@users.noreply.github.com>
2023-10-31 14:12:06 +0100

    Ejercicio 2 Python.

--- adivinanza.py
+++ adivinanza.py
@@ -1,25 +1,45 @@
-adiv_dicc = {
-    'adivinanza':'¿Cuál es el jueguete más egoísta?',
-    'ops':'a) Pelota \nb) Yo-yo \nc) Peonza',
-    'oc':'a'
-} #Instaurmaos el diccionario
+if __name__ =="__main__":
 
-print("Esto es un juego de adivinanza")
+    punt =0 #Variable de la puntuación
 
-def comprobarAdivinanza(op):#Función que comprueba la entrada y acierto/fallo
-    if op == 'a' or op == 'b' or op =='c':
-        if op == adiv_dicc['oc']:
-            print('Acertaste!')
-        else:  
-            print('Fallaste!')
-    else:
-        print("La opción debe ser una de las posibles (a,b,c)")
+    adiv1_dicc = {
+        'adivinanza':'¿Cuál es el jueguete más egoísta?',
+        'ops':'a) Pelota \nb) Yo-yo \nc) Peonza',
+        'oc':'a'
+    }
+    #Instauramos el diccionario 1
 
-print(adiv_dicc['adivinanza'])
-print(adiv_dicc['ops'])
+    adiv2_dicc = {
+        'adivinanza':'Largo largero, Martín Caballero, sin patas ni manos y corre ligero',
+        'ops':'a) El autobús \nb) El coche \nc) El tren',
+        'oc':'c'
+    } #Instauramos el diccionario 2
 
 
-op = input('Opción elegida: ')#Aceptamos la entrada del usuario
+    adiv3_dicc = {
+        'adivinanza':'Corren más que los minutos, pero nunca llegan los primeros.',
+        'ops':'a) Las horas \nb) Las agujas \nc) Los segundos',
+        'oc':'c'
+    } #Instauramos el diccionario 2
 
 
-comprobarAdivinanza(op)
+    dicclist= [adiv1_dicc, adiv2_dicc, adiv3_dicc]
+
+
+
+    print("Esto es un juego de adivinanza")
+        #Mostramos adivinanzas
+    for i in dicclist:
+            print(i['adivinanza'])
+            print(i['ops'])
+            op = input('Opción elegida: ')#Aceptamos la entrada del usuario
+            if op == 'a' or op == 'b' or op =='c':
+                if op == i['oc']:
+                    print('Acertaste!')
+                    punt+=10
+                else:  
+                    print('Fallaste!')
+                    punt -=5
+            else:
+                print("La opción debe ser una de las posibles (a,b,c)")
+    print("Puntuación: "+str(punt))
