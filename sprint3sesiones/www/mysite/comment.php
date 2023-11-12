351105aebb344711fc46a045c361d1639cdbffcf
aseoaneo23 <aseoaneo22@fpcoruna.afundacion.org>
2023-11-12 19:03:54 +0100

    Ejercicio 4: Modifico comment.php y evito inyecciones de sql.

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -4,17 +4,32 @@
 <html>
   <body>
     <?php
-	$id_juego = $_POST['id_juego'];
-	$comentario = $_POST['new_comment']; //guardamos el comment en una variable	
+	session_start();//Inicio de manipulación de la sesión
+	$id_insertar = 'NULL';//Creamos variable para insert.
 
-	//guardamos la consulta en una variable.
-	$query = "INSERT INTO tComentarios(comentario, usuario_id, juego_id, timestamp) VALUES ('".$comentario."',NULL,".$id_juego.",now())";
+	if (!empty($_SESSION['userId'])){
+		$id_user_insertar = $_SESSION['userId'];
 
-	mysqli_query($db, $query) or die('Error');
+		$id_juego = $_POST['id_juego'];
+		$comentario = $_POST['new_comment']; //guardamos el comment en una variable	
 
-	echo "<p>Nuevo comentario nº ";
-	echo mysqli_insert_id($db);
-	echo " añadido</p>";
+		//guardamos la consulta en una variable.
+		$query = $db->prepare("INSERT INTO tComentarios(comentario, usuario_id, juego_id, timestamp) VALUES (?,?,?,now())");
+		$query->bind_param('sii',$comentario,$id_user_insertar,$id_juego);		echo'hola';
+
+		$query->execute();
+
+
+		//mysqli_query($db, $query) or die('Error');
+
+		echo "<p>Nuevo comentario nº ";
+		echo mysqli_insert_id($db);
+		echo " añadido</p>";
+	} else {
+		echo '<p>No puedes insertar un comentario porque no estás logueado</p>';
+
+	}
+
 
 	echo "<a href='/main.php'>Volver</a>";
 	mysqli_close($db);
