e84b2ac7cdfc4ac167773d99eaf42f780297785b
aseoaneo23 <aseoaneo22@fpcoruna.afundacion.org>
2023-11-23 10:53:04 +0100

    Comentarios de código añadidos.

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -6,12 +6,18 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 # Create your views here.
 
+#Función que muestra un h1 por pantalla 
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>HOLA</h1>");
 
-def devolver_juegos(request):
+#Función que devuelve un array el cuál contiene objetos con todas las columnas
+# de la base de datos. 
+def devolver_juegos(request)
+#En lista se guardan todos los datos de la base de datos juegos:
 	lista = Tjuegos.objects.all()
 	respuesta_final = []
+#Bucle que por cada fila de datos de la base cree un objeto y lo añade al array
+#respuesta_final
 	for fila_sql in lista:
 		diccionario = {}
 		diccionario['id']=fila_sql.id
@@ -22,10 +28,15 @@ def devolver_juegos(request):
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe=False)
 
+#Función que devuelve un objeto JSON con el id que se le solicita por URL 
 def devolver_juego_por_id(request,id_solicitado):
+#Se guradan datos en juego de el id solicitado
 	juego = Tjuegos.objects.get(id = id_solicitado)
+#Se guardan en comentarios los elementos de la tabla Tcomentarios asociados con 
+#el mismo id relacionado con la Foreign Key.
 	comentarios = juego.tcomentarios_set.all()
 	lista_comentarios = []
+#Bucle que crea un objeto por cada comentario y para el juego solicitado
 	for fila_comentario_sql in comentarios:
 		diccionario ={}
 		diccionario['id'] = fila_comentario_sql.id
@@ -42,13 +53,18 @@ def devolver_juego_por_id(request,id_solicitado):
 	return JsonResponse(resultados, json_dumps_params={'ensure_ascii':False})
 
 @csrf_exempt
+#Función que en caso de recibir una petición POST, introduce los datos de comentario.
 def guardar_comentario(request, id_juego):
 	if request.method != 'POST':
 		return None
-
+#Cargamos la petición
 	json_peticion = json.loads(request.body)
+#Guardamos en comentario la tabla Tcomentarios
 	comentario = Tcomentarios()
+#guardamos en la columna comentario el comentario pasado por el POST
 	comentario.comentario = json_peticion['nuevo_comentario']
+#guardamos en el id de juego el id correspondiente
 	comentario.juego = Tjuegos.objects.get(id = id_juego)
+#Guardamos los cambios de la inserción
 	comentario.save()
 	return JsonResponse({"status":"ok"}) 
